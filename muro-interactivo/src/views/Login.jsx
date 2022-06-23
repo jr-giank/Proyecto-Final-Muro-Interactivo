e1a76cac28efbb50fca67276481dcdc5684ddd15
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-06-22 20:24:54 -0400

    Avance form login

--- muro-interactivo/src/views/Login.jsx
+++ muro-interactivo/src/views/Login.jsx
@@ -7,16 +7,17 @@ export const Login = () => {
             <section id="log-in">
                 <form action="">
                     <h2>Bienvenido</h2>
+                    <h4>¡Por favor completa la información!</h4>
                     <div className="log-info">
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email" id="first-one">Email</label>
                         <input type="email"/>
                     </div>
                     <div className="log-info">
                         <label htmlFor="">Password</label>
                         <input type="password" />
                     </div>
-                    <button>Sign in</button>
-                    <button>Sign in with Google</button>
+                    <button id="sign-in-btn">Sign in</button>
+                    <button id="google-btn"><img alt="Google" src="https://img.icons8.com/color/22/000000/google-logo.png"/>Sign in with Google</button>
                 </form>
             </section>
             <section id="image">
