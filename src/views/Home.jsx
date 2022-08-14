f4b62bb22814248bc668efd234edac3c55af5a03
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-08-14 16:50:55 -0400

    Cambio carpeta .git a raiz del proyecto

---
+++ src/views/Home.jsx
@@ -0,0 +1,151 @@
+import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, firestore } from "../firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { listAll, ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../firebase/firebase";
+import { Autentication } from "../services/Autentication";
+import "../styles/Home.css";
+
+export const Home = () => {
+    const [user, setUser] = useState({});
+    const [userDetails, setUserDetails] = useState([]);
+    const [postsDetails, setPostsDetails] = useState([]);
+    
+    const { logoutService } = Autentication();
+    const profilesPhotoRef = ref(storage, 'profiles/');
+    const postPhotoRef = ref(storage, 'posts/');
+    const navigate = useNavigate();
+
+    const handleLogout = (auth) => {
+        logoutService(auth);
+
+        navigate('/login');
+    }
+    
+    const getDetailsProfile = async() => {
+        try {
+            const query = await getDocs(collection(firestore, 'users'));
+            const data = [];
+
+            query.forEach((doc) => {
+                if(doc.id === user.uid){
+                    listAll(profilesPhotoRef).then((response) => {
+                        response.items.forEach((item) => {
+                            if(item._location.path === `profiles/${user.uid}`){
+                                getDownloadURL(item).then((photo) => {
+                                    data.push({...doc.data(), photo:photo, id:doc.id})
+                                    setUserDetails(data);
+                                })
+                            }
+                        })
+                    })
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getDetailsPosts = async() => {
+        try {
+            const query = await getDocs(collection(firestore, 'posts'));
+            const data = [];
+
+            query.forEach((doc) => {
+                listAll(postPhotoRef).then((response) => {
+                    response.items.forEach((item) => {
+                        if(item._location.path === `posts/${doc.id}`){
+                            getDownloadURL(item).then((post_photo) => {
+                                listAll(profilesPhotoRef).then((response) => {
+                                    response.items.forEach((item) => {
+                                        if(item._location.path === `profiles/${doc.data().uid_profile_photo}`){
+                                            getDownloadURL(item).then((profile_photo) => {
+                                                data.push({...doc.data(), post_photo:post_photo, profile_photo:profile_photo, id:doc.id});
+                                                console.log(data);
+                                                setPostsDetails(data);
+                                            })
+                                        }
+                                    })
+                                })
+                            })
+                        }
+                    })
+                })
+            })
+            // getDetailsProfile();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+
+        getDetailsProfile();
+        getDetailsPosts();
+    }, [setUser])
+
+    // getDetailsProfile();
+    // getDetailsPosts();
+
+    return(        
+        user ? 
+            <div id="HomePage">      
+                <section id="profile"> 
+                    {
+                        userDetails ? 
+                            userDetails.map((user) => (
+                                <div id="content" key={user.id}>
+                                    <img src={user.photo} alt="Photo"/>
+                                    <p><strong>{user.name} {user.last_name}</strong></p>
+                                    <p>{user.username}</p>
+                                </div>
+                            )) 
+                        : <h1>Holsss</h1>
+                    }
+                    
+                    <div id="buttons">
+                        <button id="new_post" onClick={() => {navigate('/new/post')}}>New post</button>
+                        <button id="logout" onClick={() => {handleLogout(auth)}}>Logout</button>
+                    </div>
+                </section>
+                
+                <section id="posts">
+                    { 
+                        postsDetails.map((post) => (
+                            <div id="post">
+                                <div key={post.id}>
+                                    <div id="post_profile">
+                                        <img src={post.profile_photo} alt="Photo"/> <h3>{post.name}</h3>
+                                    </div>
+                                    
+                                    <img id="foto_post" alt="Photo post" src={post.post_photo}/>
+                                    <p>{post.text}</p>
+                                </div>
+                            </div>
+                        ))   
+                    }
+                </section>
+            </div>
+        : 
+            <div id="HomePage">
+                <section id="posts">
+                    <div>
+                        <div id="profile-post">
+                            <p>foto</p>
+                            <p>Nombre Persona</p>
+                        </div>
+                        <div id="post">
+                            {/* <img id="foto_preview" name='foto_preview' src={preview} alt="" /> */}
+                            <p id="foto_preview">Aqui va la foto</p>
+                            <p>textto</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+    )
+}
